d53467c86db8d655b85a4906ae0456dfaa91cd84
liaoinstan <liaoinstan@qq.com>
2016-06-08 15:11:58 +0800

    init commit

--- tvcust/src/main/java/com/sobey/tvcust/ui/activity/TestActivity.java
+++ tvcust/src/main/java/com/sobey/tvcust/ui/activity/TestActivity.java
@@ -3,19 +3,30 @@ package com.sobey.tvcust.ui.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.shelwee.update.UpdateHelper;
+import com.shelwee.update.listener.OnUpdateListener;
+import com.shelwee.update.pojo.UpdateInfo;
 import com.sobey.common.utils.ACache;
 import com.sobey.common.utils.ClearCacheUtil;
 import com.sobey.tvcust.R;
 
 public class TestActivity extends AppCompatActivity {
+    UpdateHelper updateHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        updateHelper = new UpdateHelper.Builder(this)
+                .checkUrl("http://xx")
+                        .isAutoInstall(false) //设置为false需在下载完手动点击安装;默认值为true，下载后自动安装。
+//                        .isHintNewVersion(false)
+                .build();
     }
 
     public void onClick(View v){
@@ -39,6 +50,34 @@ public class TestActivity extends AppCompatActivity {
                 break;
             case R.id.acache:
                 ACache.get(this).put("test","asdasdada");
+                break;
+            case R.id.version:
+                updateHelper.check(new OnUpdateListener() {
+                    @Override
+                    public void onStartCheck() {
+                        Log.e("liao","onStartCheck");
+                    }
+                    @Override
+                    public void onFinishCheck(UpdateInfo info) {
+                        Log.e("liao","onFinishCheck");
+                    }
+                    @Override
+                    public void onStartDownload() {
+                        Log.e("liao","onStartDownload");
+                    }
+                    @Override
+                    public void onInstallApk() {
+                        Log.e("liao","onInstallApk");
+                    }
+                    @Override
+                    public void onFinshDownload() {
+                        Log.e("liao","onFinshDownload");
+                    }
+                    @Override
+                    public void onDownloading(int progress) {
+//                        Log.e("liao","onDownloading:"+progress);
+                    }
+                });
                 break;
         }
     }
